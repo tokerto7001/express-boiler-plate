252254350084292fa6664be61504ff50f555c3ee
Hasan Toker <75892548+tokerto7001@users.noreply.github.com>
2023-01-21 11:26:16 +0300

    ADD minor produciron settings for middlewares

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,25 @@ import authRoutes from './routes/authRoutes';
 import cookieParser from "cookie-parser"
 import { AppError } from './utils/appError';
 import { errorHandler } from './utils/errorHandler';
+import { whiteList } from './config/whiteList';
 
 const app: Application = express();
 
-app.use(cors());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+    app.use(cors());
+} else {
+    // log more detailed info about the incoming request
+    app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+    // restrict the domains which can interact with this API
+    app.use(cors(
+        {
+            origin: whiteList,
+            methods: "GET,HEAD,PUT,PATCH,POST,DELETE"
+        }
+    ));
+}
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
